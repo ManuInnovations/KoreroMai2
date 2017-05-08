97a1cf49592f0963264abf434f46d5658ac0d892
anna-marie-vos <amvos@outlook.com>
2017-05-08 21:28:04 +1200

    updated route, changing state.index.js with route and letters

--- state/index.js
+++ state/index.js
@@ -1,20 +1,21 @@
 module.exports = {
 
   letters: [
-   1: {
+   {
      id: 1,
      capital: 'A',
      lowercase: 'a'
    },
-   2: {
+   {
      id: 2,
      capital: 'E',
      lowercase: 'e'
    },
-   3: {
+   {
      id: 3,
      capital: 'H',
      lowercase: 'h'
    }
- ]
+ ],
+ route:{}
 }
