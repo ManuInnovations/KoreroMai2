00cb80cb4808537c363c8b333328726fbdd9f8e4
anna-marie-vos <amvos@outlook.com>
2017-05-25 12:24:40 +1200

    media file also rendering

--- src/index.js
+++ src/index.js
@@ -38,9 +38,8 @@ const Root = ({ store }) => {
             <Route path="/letterindex/:id" component={Displayletter} />
             <Route path="/letterindex/:id/watch" component={Mediashow}/>
           </Route>
-          <Route path="/media" component={Medialibrary} >
-            <Route path="/media/:id" component={Mediashow} />
-          </Route> 
+          <Route path="/media" component={Medialibrary} />
+          <Route path="/media/:id" component={Mediashow} />
           <Route path="/privacy" component={Privacy} />
           <Route path="/about" component={About} />
         </Route>
