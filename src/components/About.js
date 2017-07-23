a6f9497f6ebb12798a3c5891059e9948be5305ca
Janie Tito <jtito@hotmail.com>
2017-07-24 09:16:12 +1200

    started tablet view

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
+import MobileViewFooter from './MobileViewFooter.js'
 
 class About extends React.Component {
   handleClick(e) {
@@ -10,52 +11,52 @@ class About extends React.Component {
   render() {
     return (
       <div className='about'>
+        <div className='about-box'>
           <h2 className='about-heading'>About Us</h2>
           <h3>Kia Ora! Kōrero Mai was made with aroha by the team at Manu, and with the assistance of the talented UX designer, Katie Deller.</h3>
-        <div className='profile'>
-          <img src='images/janie.png' alt='Janie Tito' />
-          <p><b>Janie Tito</b></p>
-          <p>Ngā Puhi me Ngāti Whātua</p>
+          <div className='profile'>
+            <img src='images/janie.png' alt='Janie Tito' />
+            <p><b>Janie Tito</b></p>
+            <p>Ngā Puhi me Ngāti Whātua</p>
+          </div>
+          <div className='profile'>
+            <img src='images/anna.png' alt='Anna-Marie Vos' />
+            <p><b>Anna-Marie Vos</b></p> <p>Āwherika ki te Tonga</p>
+          </div>
+          <div className='profile'>
+            <img src='images/meghan.png' alt='Meghan Hughes' />
+            <p><b>Meghan Hughes</b></p>
+            <p>Ngāti Pākehā</p>
+          </div>
+          <div className='profile'>
+            <img src='images/katie.png' alt='Katie Deller' />
+            <p><b>Katie Deller</b></p>
+            <p>Ngāti Porou</p>
+          </div>
+          <div className='purpose'>
+            <h3>How this came about</h3>
+            <p>Janie has two tamariki who
+            attend bilingual classes at Koraunui
+            Primary School in Lower Hutt. One day
+            their teacher shared her wish for an
+            app to help her students learn te reo
+            Māori phonics - and this sparked the
+            idea for Kōrero Mai.</p>
+            <p>Koraunui Primary School has been a
+            huge help in the development of this
+            app, as well as providing voices and
+            video footage. Ngā mihi nui ki a
+            koutou!</p>
+          </div>
+          <div className='contact'>
+            <h4>Please get in touch,
+            Email: hello@manu.net.nz</h4>
+            <h4>To find out more about
+            Manu: <a href='https://www.manu.net.nz'>www.manu.net.nz</a></h4>
+            <h4>To contact Katie, Email: kdellerdesign@gmail.com</h4>
+          </div>
         </div>
-        <div className='profile'>
-          <img src='images/anna.png' alt='Anna-Marie Vos' />
-          <p><b>Anna-Marie Vos</b></p> <p>Āwherika ki te Tonga</p>
-        </div>
-        <div className='profile'>
-          <img src='images/meghan.png' alt='Meghan Hughes' />
-          <p><b>Meghan Hughes</b></p>
-          <p>Ngāti Pākehā</p>
-        </div>
-        <div className='profile'>
-          <img src='images/katie.png' alt='Katie Deller' />
-          <p><b>Katie Deller</b></p>
-          <p>Ngāti</p>
-        </div>
-        <div className='purpose'>
-          <h3>How this came about</h3>
-          <p>Janie has two tamariki who
-          attend bilingual classes at Koraunui
-          Primary School in Lower Hutt. One day
-          their teacher shared her wish for an
-          app to help her students learn te reo
-          Māori phonics - and this sparked the
-          idea for Kōrero Mai.</p>
-          <p>Koraunui Primary School has been a
-          huge help in the development of this
-          app, as well as providing voices and
-          video footage. Ngā mihi nui ki a
-          koutou!</p>
-        </div>
-        <div className='contact'>
-          <h4>Please get in touch,
-          Email: hello@manu.net.nz</h4>
-          <h4>To find out more about
-          Manu: <a href='https://www.manu.net.nz'>www.manu.net.nz</a></h4>
-          <h4>To contact Katie, Email: dellerka@gmail.com</h4>
-          <Link to='/letters'>
-            <p>Back to Contents</p>
-          </Link>
-        </div>
+        <MobileViewFooter />
       </div>
     )
   }
