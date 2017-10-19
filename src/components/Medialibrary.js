18ec0cfa66d492a246f57050f385d4e9ec481730
Janie Tito <jtito@hotmail.com>
2017-10-19 23:33:22 +1300

    media page images in a grid layout

--- src/components/Medialibrary.js
+++ src/components/Medialibrary.js
@@ -18,27 +18,31 @@ class Medialibrary extends React.Component {
       <div className='medialibrary-container'>
         <Header />
         <div className='medialibrary-box'>
-          <h2>Media Library</h2>
+          <h2>Ataata</h2>
+          <hr />
+          <h3>Choose a video to watch</h3>
+          <div className="medialibrary-images">
           {
             letters.map((letter, index) => {
               return (
-                  <div className='media-image' key={index}>
-                    <Link key={letter.id} to={`/media/${letter.capital}`}>
-                      <img
-                        key={index}
-                        src={`${letter.wordImage[1].image}`}
-                        className='img-fluid'
-                        onClick={() =>
-                          dispatch({
-                            type: 'RENDER_LETTER',
-                              payload: letter,
-                            })
-                        } />
-                    </Link>
-                  </div>
+                <div className='media-image' key={index}>
+                  <Link key={letter.id} to={`/media/${letter.capital}`}>
+                    <img
+                      key={index}
+                      src={`${letter.wordImage[1].image}`}
+                      className='img-fluid'
+                      onClick={() =>
+                        dispatch({
+                          type: 'RENDER_LETTER',
+                            payload: letter,
+                          })
+                      } />
+                  </Link>
+                </div>
               )
             })
           }
+          </div>
         </div>
         <Footer />
       </div>
